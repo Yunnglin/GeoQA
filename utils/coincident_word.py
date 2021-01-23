caa31d76804fc4932db8b6e7dbe90c02b48c7121
Yunnglin <mao.looper@qq.com>
2021-01-23 17:12:08 +0800

    first generating tags

--- utils/coincident_word.py
+++ utils/coincident_word.py
@@ -5,6 +5,8 @@ import jieba
 E_symbols = u',.!?[]()<>'
 C_symbols = u'，。！？【】（）《》'
 
+STOPWORDS = ['图示', '我国', '图中']
+
 
 def preprocess(sentence: str):
     # 中英符号转换
@@ -19,20 +21,18 @@ def read_json_data(file_path) -> dict:
         return f_dict
 
 
-def get_same_word(s1: str, s2: str):
+def get_same_word(s1: str, s2: str, word_len=3):
     """
     提取两个字符串的相同部分，尽量长
+    :param word_len: 标注的连续词语长度
     :param s1: 句子1
     :param s2: 句子2
     :return:
     """
-    # 处理换行空格等
-    s1 = preprocess(s1)
-    s2 = preprocess(s2)
-
     index = 0
     str_len = len(s1)
     res = list()
+    res_index = list()
     while index < str_len:
         # find_all_index = lambda sentence, character: [i for i in range(len(sentence)) if sentence[i] == character]
         # all_index = find_all_index(s2, s1[index])
@@ -44,18 +44,21 @@ def get_same_word(s1: str, s2: str):
                 if s1[index + lens] in C_symbols:
                     break
                 lens += 1
-            if lens >= 2:
-                res.append(s1[index:index + lens])
+            # 　长度大于２，且未被停用
+            if lens >= word_len:
+                word = s1[index:index + lens]
+                if word not in STOPWORDS:
+                    res.append(word)
+                    res_index.append([index, index + lens])
             index += lens
         else:
             index += 1
-    return res
+    print(s1)
+    print(s2)
+    return res, res_index
 
 
 def get_words_with_jieba(s1: str, s2: str):
-    # 处理换行空格等
-    s1 = preprocess(s1)
-    s2 = preprocess(s2)
     s1_list = list(jieba.cut(s1))
     s2_list = list(jieba.cut(s2))
     print(' '.join(s1_list))
@@ -63,16 +66,31 @@ def get_words_with_jieba(s1: str, s2: str):
     return [word for word in s1_list if word in s2_list and word not in C_symbols]
 
 
+def generate_tags(n, words_index):
+    tags = ['O'] * n
+    for index in words_index:
+        start = index[0]
+        tags[start] = 'B'
+        while start < index[1] - 1:
+            start += 1
+            tags[start] = 'I'
+    return tags
+
+
 def tagging(cut=False):
     dicts = read_json_data('../data/data_all/53_data.json')
-    for question in dicts:
-        background = question['background']
-        explain = question['explanation']
-        if cut:
-            words = get_words_with_jieba(background, explain)
-        else:
-            words = get_same_word(background, explain)
-        print(words)
+    with open('../data/train.txt', 'w', encoding='utf8') as f:
+        for question in dicts:
+            background = preprocess(question['background'])
+            explain = preprocess(question['explanation'])
+            if cut:
+                words, words_index = get_words_with_jieba(background, explain)
+            else:
+                words, words_index = get_same_word(background, explain)
+            print(words)
+            tags = (generate_tags(len(background), words_index))
+            for i in range(len(background)):
+                f.write(background[i] + '\t' + tags[i] + '\n')
 
 
 tagging(False)
