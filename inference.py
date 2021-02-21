47229569d81f41ae34d23794653ccb00701eecf6
Yunnglin <mao.looper@qq.com>
2021-02-21 17:40:12 +0800

    add inference

---
+++ inference.py
@@ -0,0 +1,114 @@
+import os
+
+import torch
+from torch.cuda import device
+from torch.utils.data import SequentialSampler, DataLoader, TensorDataset
+from transformers import BertTokenizer
+
+from bert_crf_model import BertCRF
+from config import get_argparse
+from data_process import collate_fn, CnerProcessor, convert_examples_to_features
+from metrics import Performance
+
+
+def load_and_cache_examples(args, tokenizer, processor, data_type='train'):
+    # 加载数据
+    save_processed_data = './data/{}_processed_data'.format(data_type)
+    if os.path.exists(save_processed_data):
+        print('加载数据')
+        features = torch.load(save_processed_data)
+    else:
+        label_list = processor.get_labels()
+        if data_type == 'train':
+            examples = processor.get_train_examples(args.data_dir)
+        elif data_type == 'dev':
+            examples = processor.get_dev_examples(args.data_dir)
+        else:
+            examples = processor.get_test_examples(args.data_dir)
+
+        features = convert_examples_to_features(examples=examples,
+                                                tokenizer=tokenizer,
+                                                label_list=label_list,
+                                                max_seq_length=args.max_seq_length,
+                                                cls_token_at_end=False,
+                                                pad_on_left=False,
+                                                cls_token=tokenizer.cls_token,
+                                                cls_token_segment_id=0,
+                                                sep_token=tokenizer.sep_token,
+                                                pad_token=tokenizer.convert_tokens_to_ids([tokenizer.pad_token])[0],
+                                                pad_token_segment_id=0,
+                                                )
+
+        torch.save(features, save_processed_data)
+
+    all_input_ids = torch.tensor([f.input_ids for f in features], dtype=torch.long)
+    all_input_mask = torch.tensor([f.input_mask for f in features], dtype=torch.long)
+    all_segment_ids = torch.tensor([f.segment_ids for f in features], dtype=torch.long)
+    all_label_ids = torch.tensor([f.label_ids for f in features], dtype=torch.long)
+    all_lens = torch.tensor([f.input_len for f in features], dtype=torch.long)
+    dataset = TensorDataset(all_input_ids, all_input_mask, all_segment_ids, all_lens, all_label_ids)
+    return dataset
+
+
+def load_model(num_labels, epoch_num=0):
+    try:
+        model = BertCRF(num_labels)
+        model.load_state_dict(torch.load(f'./save_model/ckpt_epoch_{epoch_num}.bin', map_location='cpu'))
+    except FileNotFoundError as e:
+        print(e)
+        print("Load Model Failed, No Such File")
+        return None
+    return model
+
+
+def evaluate(args, model, tokenizer, processor, data_type):
+    performance = Performance(id2label=args.id2label)
+    eval_dataset = load_and_cache_examples(args, tokenizer, processor=processor, data_type=data_type)
+    eval_sampler = SequentialSampler(eval_dataset)
+
+    batch_size = args.eval_batch_size if data_type == "dev" else args.predict_batch_size
+    eval_dataloader = DataLoader(eval_dataset, sampler=eval_sampler, batch_size=batch_size,
+                                 collate_fn=collate_fn)
+
+    print("***** Running %s *****" % data_type)
+    print("  Num examples = %d" % len(eval_dataset))
+    print("  Batch size = %d" % args.eval_batch_size)
+    eval_loss = 0.0
+    nb_eval_steps = 0
+
+    for step, batch in enumerate(eval_dataloader):
+        model.eval()
+        with torch.no_grad():
+            inputs = {"input_ids": batch[0], "attention_mask": batch[1], 'token_type_ids': batch[2], "labels": batch[3],
+                      'input_lens': batch[4]}
+            outputs = model(**inputs)
+            tmp_eval_loss, logits = outputs[:2]
+            tags = model.crf.decode(logits, inputs['attention_mask'])
+
+        eval_loss += tmp_eval_loss.item()
+        nb_eval_steps += 1
+        out_label_ids = inputs['labels'].cpu().numpy().tolist()  # 真实标签id
+        tags = tags.squeeze(0).cpu().numpy().tolist()  # 预测标签id
+        # 每个batch更新三项指标
+        performance.update_performance(real_label=out_label_ids, predict_label=tags)
+
+    # 输出指标
+    print("\n ***** %s results ***** " % data_type)
+    eval_loss = eval_loss / nb_eval_steps
+    performance.res_dict['eval_loss'] = eval_loss
+    print(performance)
+
+
+if __name__ == "__main__":
+    processor = CnerProcessor()
+    label_list = processor.get_labels()
+    num_labels = len(label_list)
+    # 将标签进行id映射
+    args = get_argparse().parse_args()
+    args.id2label = {i: label for i, label in enumerate(label_list)}
+    args.label2id = {label: i for i, label in enumerate(label_list)}
+
+    model = load_model(num_labels=num_labels, epoch_num=9)
+    tokenizer = BertTokenizer.from_pretrained('./bert/vocab.txt')
+
+    evaluate(args, model, tokenizer, processor=processor, data_type="test")
--- run.py
+++ run.py
@@ -3,90 +3,13 @@ import time
 from os.path import join
 
 import torch
-from torch.utils.data import TensorDataset, RandomSampler, DataLoader, SequentialSampler
+from torch.utils.data import RandomSampler, DataLoader
 from transformers import BertTokenizer, AdamW, get_linear_schedule_with_warmup
 
 from bert_crf_model import BertCRF
 from config import get_argparse
-from data_process import CnerProcessor as Processor, convert_examples_to_features, collate_fn
-from metrics import Performance
-
-
-def load_and_cache_examples(args, tokenizer, data_type='train'):
-    # 加载数据
-    save_processed_data = './data/{}_processed_data'.format(data_type)
-    if os.path.exists(save_processed_data):
-        print('加载数据')
-        features = torch.load(save_processed_data)
-    else:
-        label_list = processor.get_labels()
-        if data_type == 'train':
-            examples = processor.get_train_examples(args.data_dir)
-        elif data_type == 'dev':
-            examples = processor.get_dev_examples(args.data_dir)
-        else:
-            examples = processor.get_test_examples(args.data_dir)
-
-        features = convert_examples_to_features(examples=examples,
-                                                tokenizer=tokenizer,
-                                                label_list=label_list,
-                                                max_seq_length=args.max_seq_length,
-                                                cls_token_at_end=False,
-                                                pad_on_left=False,
-                                                cls_token=tokenizer.cls_token,
-                                                cls_token_segment_id=0,
-                                                sep_token=tokenizer.sep_token,
-                                                pad_token=tokenizer.convert_tokens_to_ids([tokenizer.pad_token])[0],
-                                                pad_token_segment_id=0,
-                                                )
-
-        torch.save(features, save_processed_data)
-
-    all_input_ids = torch.tensor([f.input_ids for f in features], dtype=torch.long)
-    all_input_mask = torch.tensor([f.input_mask for f in features], dtype=torch.long)
-    all_segment_ids = torch.tensor([f.segment_ids for f in features], dtype=torch.long)
-    all_label_ids = torch.tensor([f.label_ids for f in features], dtype=torch.long)
-    all_lens = torch.tensor([f.input_len for f in features], dtype=torch.long)
-    dataset = TensorDataset(all_input_ids, all_input_mask, all_segment_ids, all_lens, all_label_ids)
-    return dataset
-
-
-def evaluate(args, model, tokenizer, prefix=""):
-    performance = Performance(id2label=args.id2label)
-    eval_dataset = load_and_cache_examples(args, tokenizer, data_type='dev')
-    eval_sampler = SequentialSampler(eval_dataset)
-    eval_dataloader = DataLoader(eval_dataset, sampler=eval_sampler, batch_size=args.eval_batch_size,
-                                 collate_fn=collate_fn)
-
-    print("***** Running evaluation %s *****" % prefix)
-    print("  Num examples = %d" % len(eval_dataset))
-    print("  Batch size = %d" % args.eval_batch_size)
-    eval_loss = 0.0
-    nb_eval_steps = 0
-
-    for step, batch in enumerate(eval_dataloader):
-        model.eval()
-        batch = tuple(t.to(device) for t in batch)
-        with torch.no_grad():
-            inputs = {"input_ids": batch[0], "attention_mask": batch[1], 'token_type_ids': batch[2], "labels": batch[3],
-                      'input_lens': batch[4]}
-            outputs = model(**inputs)
-            tmp_eval_loss, logits = outputs[:2]
-            tags = model.crf.decode(logits, inputs['attention_mask'])
-
-        eval_loss += tmp_eval_loss.item()
-        nb_eval_steps += 1
-        out_label_ids = inputs['labels'].cpu().numpy().tolist()  # 真实标签id
-        tags = tags.squeeze(0).cpu().numpy().tolist()  # 预测标签id
-        # 每个batch更新三项指标
-        performance.update_performance(real_label=out_label_ids, predict_label=tags)
-
-    # 输出指标
-    print("\n ***** Eval results %s ***** " % prefix)
-    eval_loss = eval_loss / nb_eval_steps
-    performance.res_dict['eval_loss'] = eval_loss
-    print(performance)
-
+from data_process import CnerProcessor, collate_fn
+from inference import evaluate, load_and_cache_examples
 
 if __name__ == "__main__":
     args = get_argparse().parse_args()
@@ -96,7 +19,7 @@ if __name__ == "__main__":
     if not os.path.exists(args.output_dir):  # 输出文件
         os.mkdir(args.output_dir)
 
-    processor = Processor()
+    processor = CnerProcessor()
     label_list = processor.get_labels()
     # 将标签进行id映射
     args.id2label = {i: label for i, label in enumerate(label_list)}
@@ -111,7 +34,7 @@ if __name__ == "__main__":
 
     # Training
     if args.do_train:
-        train_dataset = load_and_cache_examples(args, tokenizer, data_type='train')
+        train_dataset = load_and_cache_examples(args, tokenizer, processor=processor, data_type='train')
         train_sampler = RandomSampler(train_dataset)
         train_dataloader = DataLoader(train_dataset, sampler=train_sampler, batch_size=args.train_batch_size,
                                       collate_fn=collate_fn)
@@ -175,7 +98,7 @@ if __name__ == "__main__":
 
                 global_step += 1
 
-            evaluate(args, model, tokenizer, prefix="")
+            evaluate(args, model, tokenizer, data_type="dev")
 
             model_to_save = model.module if hasattr(model, 'module') else model
             torch.save(model_to_save.state_dict(), join(args.checkpoint_path, "ckpt_epoch_{}.bin".format(epoch)))
